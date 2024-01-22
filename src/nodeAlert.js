45e5cae49a5c54094817a6a5eaaf9080d5c7a2d9
DanNaor <dan@shuffll.com>
2024-01-22 16:06:51 +0200

    working poc

--- src/nodeAlert.js
+++ src/nodeAlert.js
@@ -11,47 +11,47 @@ async function sendAlertToAlertmanager(alert) {
     console.error('Error sending alert to Alertmanager:', error)
   }
 }
+const startsAt = moment().toISOString(); // Use the current time as the startsAt timestamp
 
 // Example alert format
-const startsAt = moment().toISOString(); // Use the current time as the startsAt timestamp
-const alert =[
+ const alert = [
   {
     labels: {
       severity: 'critical',
       type: 'nodejs',
-      alertname: 'SomethingWentWrong1', // Include the alertname label
+      alertname: 'SomethingWentWrong', 
     },
     annotations: {
       summary: 'Something went wrong!',
       description: 'the something went really wrong!.',
     },
-    startsAt: startsAt, 
-    endsAt: moment().add(1, 'hour').toISOString(), 
+    startsAt: startsAt,
+    // endsAt: moment().add(1, 'hours').toISOString(),
     status: 'firing',
+    
   },
-];
+]
 const resolvedAlert = [
   {
-    labels: {
+    labels: { 
       severity: 'critical',
       type: 'nodejs',
-      alertname: 'SomethingWentWrong1', // Include the alertname label
-
+      alertname: 'SomethingWentWrong', 
     },
-    annotations: {
+    annotations: { 
       summary: 'Resolved: Something went wrong!',
       description: 'The issue has been resolved.',
     },
-    startsAt: startsAt, 
-    endsAt: moment().add(1, 'hours').toISOString(), 
+    // startsAt: startsAt,
+    // endsAt: moment().add(1, 'hours').toISOString(),
     status: 'resolved',
   },
-];
+]
 
 // Send the alert
 sendAlertToAlertmanager(alert)
 
-//go to http://localhost:9093/#/alerts and wait 10 sec 
-setTimeout(() => {
-sendAlertToAlertmanager(resolvedAlert)
-}, 10000);
+// //go to http://localhost:9093/#/alerts and wait 5 sec
+// setTimeout(() => {
+//   sendAlertToAlertmanager(alert)
+// }, 16000)
